722e308c33e3c2d9043322ec41bc68f28c422948
Logvynenko <wsbekerip@gmail.com>
2026-06-27 18:41:42 +0300

    Examples for "array", "type" and "object" data types

--- src/index.js
+++ src/index.js
@@ -1,25 +1,51 @@
 "use strict";
 /*
-Задача: Створити форму з двома input які запитують користувачів їх ім'я та вік.
-        У формі має бути кнопка "Купити алкоголь".
-        Якщо вік користувача дозволяє - вивід повідомлення у верстку про те, що алкоголь купувати дозволено.
-        В противному випадку - вивід повідомлення про заборону купування.
+Задача 1: Дано масив чисел. Перебрати масив та знайти найбільше число.
 */
-const form = document.querySelector('#alcohol-form');
-const messageDiv = document.querySelector("#message");
-// Зчитування вводимих користувачем даних
-form.addEventListener('submit', (event) => {
-    event.preventDefault(); // Зупинка завантаження сторінки після відправки форми
-    const nameInput = document.querySelector("#name");
-    const ageInput = document.querySelector("#age");
-    // "Витягнення" значень з input-ів
-    const name = nameInput.value;
-    const age = Number(ageInput.value); // з input-ів завжди повертається тип даних string, тому приводимо до типу даних number
-    // Реалізація логіки
-    if (age >= 18) {
-        messageDiv.textContent = `${name}, Вам дозволено купувати алкоголь.`;
+const numbers = [4, 2, 7, 1, 9, 5];
+let maxNumber = numbers[0];
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > maxNumber) {
+        maxNumber = numbers[i];
     }
-    else {
-        messageDiv.textContent = `${name}, Вам заборонено купувати алкоголь.`;
+}
+console.log('Найбільший елемент масиву: ', maxNumber);
+/*
+Задача 2: Дано масив чисел. Знайти середнє значення у цьому масиві чисел.
+ */
+const numbers2 = [10, 20, 30, 40, 50];
+let sum = 0;
+for (let num of numbers2) {
+    sum = sum + num;
+}
+const average = sum / numbers2.length;
+console.log("Середнє значення чисел у масиві: ", average);
+/*
+Задача 3: Дано масив чисел. Знайти перше від'ємне число у масиві.
+*/
+const numbers3 = [10, -20, 30, -40, 50];
+let firstNegative;
+for (let num of numbers2) {
+    if (num < 0) {
+        firstNegative = num;
+        break;
     }
-});
+}
+console.log("Перше від'ємне число у масиві: ", firstNegative);
+const item = { name: 'Футболка', price: 20 };
+const discount = 10; // 10% знижки
+const discountAmount = (item.price * discount) / 100;
+const totalCost = item.price - discountAmount;
+console.log(`Вартість покупки з урахуванням знижки: ${totalCost}$`);
+const John = { name: 'John', hourlyRate: 10 };
+const JohnAmount = John.hourlyRate * 40; // сума зарплати, яку мають виплатити Джону
+const invoice = { name: John.name, amount: JohnAmount };
+console.log(`Зарплата для ${invoice.name}: ${invoice.amount}$`);
+const books = [
+    { title: "Грокаємо алгоритми", author: "Адітья Бхаргава", price: 25 },
+    { title: "Мова програмування C++", author: "Б'ярн Страуструп", price: 20 },
+    { title: "Гаррі Поттер і філософський камінь", author: "Джоан Роулінг", price: 10 }
+];
+for (let book of books) { // змінна book на кожній ітерації приймає один об'єкт типу Book з масиву books
+    console.log(`Книга: ${book.title}. Автор: ${book.price}. Ціна: ${book.price}`);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,123 @@
 /*
-Задача: Створити форму з двома input які запитують користувачів їх ім'я та вік.
-        У формі має бути кнопка "Купити алкоголь".
-        Якщо вік користувача дозволяє - вивід повідомлення у верстку про те, що алкоголь купувати дозволено. 
-        В противному випадку - вивід повідомлення про заборону купування.
+Задача 1: Дано масив чисел. Перебрати масив та знайти найбільше число.
 */
 
-const form = document.querySelector('#alcohol-form') as HTMLFormElement;
-const messageDiv = document.querySelector("#message") as HTMLDivElement;
+const numbers: number[] = [4, 2, 7, 1, 9, 5];
 
-// Зчитування вводимих користувачем даних
-form.addEventListener('submit', (event) => {
-    event.preventDefault(); // Зупинка завантаження сторінки після відправки форми
-    const nameInput = document.querySelector("#name") as HTMLInputElement;
-    const ageInput = document.querySelector("#age") as HTMLInputElement;
+let maxNumber: number = numbers[0];
 
-    // "Витягнення" значень з input-ів
-    const name: string = nameInput.value;
-    const age: number = Number(ageInput.value); // з input-ів завжди повертається тип даних string, тому приводимо до типу даних number
-    // Реалізація логіки
-    if (age >= 18) {
-        messageDiv.textContent = `${name}, Вам дозволено купувати алкоголь.`;
-    } else {
-        messageDiv.textContent = `${name}, Вам заборонено купувати алкоголь.`;
+for (let i=0; i < numbers.length; i++) {
+
+    if (numbers[i] > maxNumber){
+        maxNumber = numbers[i];
     }
-});
+}
+
+console.log('Найбільший елемент масиву: ', maxNumber);
+
+
+
+/*
+Задача 2: Дано масив чисел. Знайти середнє значення у цьому масиві чисел.
+ */
+
+const numbers2: number[] = [10, 20, 30, 40, 50];
+
+let sum: number = 0;
+for(let num of numbers2){
+    sum = sum + num;
+}
+
+const average: number = sum / numbers2.length;
+console.log("Середнє значення чисел у масиві: ", average);
+
+
+
+/*
+Задача 3: Дано масив чисел. Знайти перше від'ємне число у масиві.
+*/
+
+const numbers3: number[] = [10, -20, 30, - 40, 50];
+let firstNegative: number | undefined;
+
+for(let num of numbers2) {
+    if (num < 0){
+        firstNegative = num;
+        break;
+    }
+}
+
+console.log("Перше від'ємне число у масиві: ", firstNegative);
+
+
+
+/*
+Задача 4: Обчислення вартості покупки з урахуванням знижки.
+            Створити програму для обчислення вартості покупки, на основі ціни товару та величини знижки
+
+            Декомпозиція:
+            1. Створити тип даних Item, який містить назву товару (name) та ціну товару(price).
+            2. Створити змінну item, яка представляє товар "Футболка" з ціною 20$.
+            3. Створити змінну, яка буде містити величину знижки у вісотках.
+            4. Обчислити вартість покупки з урахуванням знижки.
+*/
+
+type Item = {
+    name: string;
+    price: number;
+};
+
+const item: Item = { name: 'Футболка', price: 20};
+
+const discount: number = 10; // 10% знижки
+
+const discountAmount: number = (item.price * discount) / 100;
+const totalCost: number = item.price - discountAmount;
+
+console.log(`Вартість покупки з урахуванням знижки: ${totalCost}$`);
+
+
+
+/*
+Задача 5: Створити програму для обчислення оплати праці працівників на основі годинної ставки
+            та кількості годин праці.
+*/
+
+type Employee = {
+    name: string;
+    hourlyRate: number; // оплата за одну годину праці
+}
+
+type PayCheck = {
+    name: string;
+    amount: number;
+}
+
+const John: Employee = { name: 'John', hourlyRate: 10};
+const JohnAmount: number = John.hourlyRate * 40; // сума зарплати, яку мають виплатити Джону
+
+const invoice: PayCheck = {name: John.name, amount: JohnAmount};
+
+console.log(`Зарплата для ${invoice.name}: ${invoice.amount}$`);
+
+
+
+/*
+Задача 6: Створити програму книжкового магазину, яка буде відображати інформацію про наявні книги.
+*/
+
+type Book = {
+    title: string,
+    author: string,
+    price: number
+}
+
+const books: Book[] = [
+    {title: "Грокаємо алгоритми", author: "Адітья Бхаргава", price: 25},
+    {title: "Мова програмування C++", author: "Б'ярн Страуструп", price: 20},
+    {title: "Гаррі Поттер і філософський камінь", author: "Джоан Роулінг", price: 10}
+];
+
+for (let book of books) { // змінна book на кожній ітерації приймає один об'єкт типу Book з масиву books
+    console.log(`Книга: ${book.title}. Автор: ${book.price}. Ціна: ${book.price}`);
+}
