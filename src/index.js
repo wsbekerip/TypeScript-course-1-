6f769ee738404ee8de21385a1a6c6db8ed024d86
Logvynenko <wsbekerip@gmail.com>
2026-06-28 16:12:33 +0300

    Practical task (final)

--- src/index.js
+++ src/index.js
@@ -1,31 +1,82 @@
 "use strict";
 /*
-Задача на практику: Створити програму для обчислення вартості замовлення в інтернет-магазині.
-                    Декомпозиція:
-                    1. Створити тип даних для представлення товару з назвою та ціною.
-                    2. Створити функцію для обчислення загальної вартості замовлення, яка приймає масив товарів, як аргумент, та повертає загальну суму.
-                        Примітка: Перевірити, що масив товарів не порожній, а ціна кожного товару більше нуля.
+ПРАКТИЧНЕ ЗАВДАННЯ
+
+2. HTML-структура:    Створити файл index.html з <button> для запуску скрипта,
+                                    <select> для вибору кольору сторінки
+                                    та порожнім блоком <div> для виведення результату.
+
+3. Робота у файлі TypeScript:   Оголосити Enum із трьома кольорами.
+                                Створити змінні: імʼя (string), вік (number).
+                                Створити функцію, яка приймає ім'я користувача і вік, та повертає рядок-привітання.
+
+                                Обробка подіЇ натисканням на кнопку:
+                                    1) Виведення у створений div привітання користувача.
+                                    2) Перебрати Enum у циклі і вивести всі його значення у вигляді списку.
+                                    3) Змінити колір фону сторінки на основі обраного значення у select.
+
+4. Компіляція та перевірка: Скомпілювати TypeScript-код у JavaScript.
+                            Підключити зкомпільований файл у HTML і перевірити роботу скрипта в браузері.
+
+5. * Додаткове завдання
+    Додати ще два поля вводу:   одне <input type="text"> для введення хобі,
+                                одне <input type="number"> для введення кількості років досвіду у цьому хобі.
+
+
+Зміни функцію привітання так, щоб вона враховувала ім’я, вік, хобі та досвід, і повертала розширене повідомлення.
+
+Використай умовну логіку:
+
+- якщо досвід більше 5 років → "Вау, ти справжній експерт у [хобі]!"
+
+- якщо досвід від 1 до 5 років → "Чудово, ти вже маєш досвід у [хобі]."
+
+- якщо 0 або менше → "Все попереду! Починати нове хобі — це цікаво."
+
+
+Приклад виводу у div:
+
+"Привіт, Аліно! Тобі 22 роки. Твоє хобі — фотографія. Вау, ти справжній експерт у фотографія!"
 */
-function calcOrderTotal(products) {
-    if (products.length === 0) {
-        console.log(`Замовлення порожнє`);
-        return 0;
+var Colors;
+(function (Colors) {
+    Colors["Blue"] = "\u0411\u043B\u0430\u043A\u0438\u0442\u043D\u0438\u0439";
+    Colors["Green"] = "\u0417\u0435\u043B\u0435\u043D\u0438\u0439";
+    Colors["Orange"] = "\u041F\u043E\u043C\u0430\u0440\u0430\u043D\u0447\u0435\u0432\u0438\u0439";
+})(Colors || (Colors = {}));
+// Знаходження елементів сторінки
+const form = document.querySelector(".form");
+const msgDiv = document.querySelector("#msg");
+const colorSelect = document.querySelector("#colors");
+// Функція, яка приймає ім'я користувача і вік, та повертає рядок-привітання (form submit)
+function greeting(name, age, hobby, experience) {
+    let hobbyMessage;
+    if (experience > 5) {
+        hobbyMessage = `Вау, ти справжній експерт у ${hobby}!`;
     }
-    let total = 0;
-    for (const product of products) { // product - поточний товар ітерації оброблення масиву
-        if (product.price <= 0) {
-            console.log(`Некоректна ціна для товару "${product.name}"`);
-            continue; // пропускаємо товар з некоректною ціною і продовжуємо перебір масиву products
-        }
-        total = total + product.price;
+    else if (experience >= 1) {
+        hobbyMessage = `Чудово, ти вже маєш досвід у ${hobby}.`;
     }
-    return total;
+    else {
+        hobbyMessage = "Все попереду! Починати нове хобі — це цікаво.";
+    }
+    return `Привіт, ${name}! Тобі ${age} років. Твоє хобі - ${hobby}. ${hobbyMessage}`; // лише повертає рядок
 }
-const products = [
-    { name: "Футболка", price: 25 },
-    { name: "Шорти", price: 30 },
-    { name: "Носки", price: -5 }
-];
-const orderTotal = calcOrderTotal(products);
-console.log(orderTotal);
-//console.log(calcOrderTotal(products));
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // Зупинка завантаження сторінки після відправки форми
+    const nameInput = document.querySelector("#name");
+    const ageInput = document.querySelector("#age");
+    const hobbyInput = document.querySelector("#hobby");
+    const experienceInput = document.querySelector("#experience");
+    // "Витягнення" значень з input-ів та select-у
+    const name = nameInput.value;
+    const age = Number(ageInput.value); // з input-ів завжди повертається тип даних string, тому приводимо до типу даних number
+    const hobby = hobbyInput.value;
+    const experience = Number(experienceInput.value);
+    msgDiv.textContent = greeting(name, age, hobby, experience);
+    // Перебір enum та зміна фону сторінки на основі обраного значення у select
+    for (const color of Object.values(Colors)) {
+        console.log(color);
+    }
+    document.body.style.backgroundColor = colorSelect.value;
+});
--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,95 @@
-/*
-Задача на практику: Створити програму для обчислення вартості замовлення в інтернет-магазині.
-                    Декомпозиція:
-                    1. Створити тип даних для представлення товару з назвою та ціною.
-                    2. Створити функцію для обчислення загальної вартості замовлення, яка приймає масив товарів, як аргумент, та повертає загальну суму.
-                        Примітка: Перевірити, що масив товарів не порожній, а ціна кожного товару більше нуля.
+/* 
+ПРАКТИЧНЕ ЗАВДАННЯ
+
+2. HTML-структура:    Створити файл index.html з <button> для запуску скрипта, 
+                                    <select> для вибору кольору сторінки 
+                                    та порожнім блоком <div> для виведення результату.
+
+3. Робота у файлі TypeScript:   Оголосити Enum із трьома кольорами.
+                                Створити змінні: імʼя (string), вік (number).
+                                Створити функцію, яка приймає ім'я користувача і вік, та повертає рядок-привітання.
+
+                                Обробка подіЇ натисканням на кнопку:
+                                    1) Виведення у створений div привітання користувача.
+                                    2) Перебрати Enum у циклі і вивести всі його значення у вигляді списку.
+                                    3) Змінити колір фону сторінки на основі обраного значення у select.
+
+4. Компіляція та перевірка: Скомпілювати TypeScript-код у JavaScript.
+                            Підключити зкомпільований файл у HTML і перевірити роботу скрипта в браузері.
+
+5. * Додаткове завдання
+    Додати ще два поля вводу:   одне <input type="text"> для введення хобі,
+                                одне <input type="number"> для введення кількості років досвіду у цьому хобі.
+
+
+Зміни функцію привітання так, щоб вона враховувала ім’я, вік, хобі та досвід, і повертала розширене повідомлення.
+
+Використай умовну логіку:
+
+- якщо досвід більше 5 років → "Вау, ти справжній експерт у [хобі]!"
+
+- якщо досвід від 1 до 5 років → "Чудово, ти вже маєш досвід у [хобі]."
+
+- якщо 0 або менше → "Все попереду! Починати нове хобі — це цікаво."
+
+
+Приклад виводу у div:
+
+"Привіт, Аліно! Тобі 22 роки. Твоє хобі — фотографія. Вау, ти справжній експерт у фотографія!"
 */
 
-type Product = {
-    name: string,
-    price: number
+enum Colors {
+    Blue = 'Блакитний',
+    Green = 'Зелений',
+    Orange = 'Помаранчевий'
 }
 
-function calcOrderTotal(products: Array<Product>): number {
-    if (products.length === 0) {
-        console.log(`Замовлення порожнє`);
-        return 0;
-    } 
+// Знаходження елементів сторінки
+const form = document.querySelector(".form") as HTMLFormElement;
+const msgDiv = document.querySelector("#msg") as HTMLDivElement;
+const colorSelect = document.querySelector("#colors") as HTMLSelectElement;
 
-    let total = 0;
-    for (const product of products) {  // product - поточний товар ітерації оброблення масиву
-        if (product.price <= 0) {
-            console.log(`Некоректна ціна для товару "${product.name}"`);
-            continue;  // пропускаємо товар з некоректною ціною і продовжуємо перебір масиву products
-        }
+// Функція, яка приймає ім'я користувача і вік, та повертає рядок-привітання (form submit)
+function greeting(name: string, age: number, hobby: string, experience: number): string {
 
-        total = total + product.price;
+    let hobbyMessage: string;
+    
+    if (experience > 5) {
+        hobbyMessage = `Вау, ти справжній експерт у ${hobby}!`;
+    } else if (experience >= 1) {
+        hobbyMessage = `Чудово, ти вже маєш досвід у ${hobby}.`;
+    } else {
+        hobbyMessage = "Все попереду! Починати нове хобі — це цікаво.";
     }
 
-    return total;
+    return `Привіт, ${name}! Тобі ${age} років. Твоє хобі - ${hobby}. ${hobbyMessage}`; // лише повертає рядок
 }
 
-const products: Array<Product> = [
-    {name: "Футболка", price: 25},
-    {name: "Шорти", price: 30},
-    {name: "Носки", price: -5}
-];
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // Зупинка завантаження сторінки після відправки форми
 
-const orderTotal = calcOrderTotal(products);
-console.log(orderTotal);
 
-//console.log(calcOrderTotal(products));
+    const nameInput = document.querySelector("#name") as HTMLInputElement;
+    const ageInput = document.querySelector("#age") as HTMLInputElement;
+    const hobbyInput = document.querySelector("#hobby") as HTMLInputElement;
+    const experienceInput = document.querySelector("#experience") as HTMLInputElement;
+
+
+    // "Витягнення" значень з input-ів та select-у
+    const name: string = nameInput.value;
+    const age: number = Number(ageInput.value); // з input-ів завжди повертається тип даних string, тому приводимо до типу даних number
+    const hobby: string = hobbyInput.value;
+    const experience: number = Number(experienceInput.value);
+
+    msgDiv.textContent = greeting(name, age, hobby, experience);
+
+    // Перебір enum та зміна фону сторінки на основі обраного значення у select
+    for (const color of Object.values(Colors)) {
+        console.log(color);
+    }
+
+    document.body.style.backgroundColor = colorSelect.value;
+})
+
+
+
